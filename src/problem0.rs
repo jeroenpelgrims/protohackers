46020c66580f8a639b60d438dfc9a78952b08caa
Jeroen Pelgrims <git@jeroenpelgrims.com>
2025-08-18 14:21:59 +0000

    Simplify problem 0 code, setup problem 1

---
+++ src/problem0.rs
@@ -0,0 +1,40 @@
+use std::io::{Read, Write};
+use std::net::Shutdown;
+use std::net::{Ipv4Addr, SocketAddrV4};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+const ADDR: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
+const PORT: u16 = 1337;
+
+fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind(SocketAddrV4::new(ADDR, PORT))?;
+
+    println!("Problem 0 listening on TCP {:?}:{:?}", ADDR, PORT);
+
+    for stream in listener.incoming() {
+        match stream {
+            Err(err) => println!("Connection failed: {:?}", err),
+            Ok(stream) => {
+                println!("Connection from: {:?}", stream.peer_addr().unwrap());
+                thread::spawn(move || {
+                    let peer_addr = stream.peer_addr().unwrap();
+                    let result = handle_client(stream);
+                    if result.is_err() {
+                        println!("An error occurred with peer: {}", peer_addr);
+                    }
+                });
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let mut buffer = Vec::new();
+    stream.read_to_end(&mut buffer)?;
+    stream.write_all(&buffer)?;
+    stream.shutdown(Shutdown::Both)?;
+    Ok(())
+}
