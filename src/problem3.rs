2994393ac5f3d3764732dd210d51583ee035d75f
Jeroen Pelgrims <git@jeroenpelgrims.com>
2025-08-23 13:07:30 +0000

    Simplify code a bit for problem 3

--- src/problem3.rs
+++ src/problem3.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
-use std::io::{BufRead, Error, ErrorKind};
-use std::io::{BufReader, Read, Write};
+use std::io::{BufRead, ErrorKind};
+use std::io::{BufReader, Write};
 use std::net::SocketAddr;
 use std::net::{Ipv4Addr, Shutdown, SocketAddrV4};
 use std::net::{TcpListener, TcpStream};
@@ -18,7 +18,7 @@ struct ConnectionInfo {
 }
 
 lazy_static::lazy_static! {
-    static ref USERS: Arc<Mutex<HashMap<SocketAddr, ConnectionInfo>>> = Arc::new(Mutex::new(HashMap::new()));
+    static ref JOINED_USERS: Arc<Mutex<HashMap<SocketAddr, ConnectionInfo>>> = Arc::new(Mutex::new(HashMap::new()));
 }
 
 fn main() -> Result<(), std::io::Error> {
@@ -42,26 +42,15 @@ fn handle_connection(stream: TcpStream) {
         .peer_addr()
         .expect("Error getting peer_addr from stream");
 
-    match join(peer_addr, &stream) {
-        Ok(()) => (),
-        Err(_) => {
-            println!("Failed to join: {:?}", peer_addr.clone());
-            disconnect(&stream);
-            return;
-        }
+    if let Err(err) = join(peer_addr, &stream) {
+        println!("Failed to join: {:?}, error: {:?}", peer_addr.clone(), err);
+        disconnect(&stream);
+        return;
     }
 
-    loop {
-        let Ok(stream) = get_stream(&peer_addr) else {
-            break;
-        };
-        let Ok(message) = read_message(&stream) else {
-            break;
-        };
+    while let Ok(message) = read_message(&stream) {
         if !message.is_empty() {
-            let Ok(()) = broadcast_message(&peer_addr, &message, false) else {
-                break;
-            };
+            let _ = broadcast_message(&peer_addr, &message, false);
         }
     }
 
@@ -85,26 +74,15 @@ fn join(peer_addr: SocketAddr, stream: &TcpStream) -> Result<(), std::io::Error>
     println!("User {} joined from {:?}", username, peer_addr);
 
     let connected_usernames = {
-        let users = USERS.lock().unwrap();
+        let users = JOINED_USERS.lock().unwrap();
         users
             .values()
             .map(|u| u.username.clone())
             .collect::<Vec<_>>()
     };
 
-    broadcast_message(
-        &peer_addr,
-        &format!("{} has entered the room", username),
-        true,
-    )?;
-    send_message(
-        &stream,
-        &format!("* The room contains: {}", connected_usernames.join(", ")),
-    )?;
-
     {
-        let mut users = USERS.lock().unwrap();
-        users.insert(
+        JOINED_USERS.lock().unwrap().insert(
             peer_addr,
             ConnectionInfo {
                 username: username.clone(),
@@ -113,17 +91,29 @@ fn join(peer_addr: SocketAddr, stream: &TcpStream) -> Result<(), std::io::Error>
         );
     }
 
+    broadcast_message(
+        &peer_addr,
+        &format!("{} has entered the room", username),
+        true,
+    );
+    send_message(
+        &stream,
+        &format!("* The room contains: {}", connected_usernames.join(", ")),
+    )?;
+
     Ok(())
 }
 
 fn disconnect(stream: &TcpStream) {
     let result = stream.shutdown(Shutdown::Both);
     match result {
-        Ok(()) => println!("Disconnected: {:?}", stream.peer_addr()),
         Err(err) => println!("Error disconnecting: {:?}", err),
+        Ok(()) => {
+            println!("Disconnected: {:?}", stream.peer_addr());
+            let mut users = JOINED_USERS.lock().unwrap();
+            users.remove(&stream.peer_addr().unwrap());
+        }
     }
-    let mut users = USERS.lock().unwrap();
-    users.remove(&stream.peer_addr().unwrap());
 }
 
 fn part(peer_addr: &SocketAddr) {
@@ -134,13 +124,13 @@ fn part(peer_addr: &SocketAddr) {
 
     let username = get_username(peer_addr).unwrap_or_default();
     let part_message = format!("{} has left the room", username);
-    broadcast_message(peer_addr, &part_message, true).unwrap();
+    broadcast_message(peer_addr, &part_message, true);
 
     disconnect(&stream);
 }
 
 fn get_stream(peer_addr: &SocketAddr) -> Result<TcpStream, std::io::Error> {
-    let users = USERS.lock().unwrap();
+    let users = JOINED_USERS.lock().unwrap();
 
     let user_info = users.get(&peer_addr).ok_or_else(|| {
         std::io::Error::new(
@@ -153,13 +143,8 @@ fn get_stream(peer_addr: &SocketAddr) -> Result<TcpStream, std::io::Error> {
 }
 
 fn send_message(mut stream: &TcpStream, message: &str) -> Result<(), std::io::Error> {
-    let result = stream.write_all(format!("{message}\n").as_bytes());
-    if let Err(err) = result {
-        part(&stream.peer_addr().unwrap());
-        return Err(err);
-    }
-    println!("Sent message: {}", message);
-    Ok(())
+    println!("Sending message: {}", message);
+    stream.write_all(format!("{message}\n").as_bytes())
 }
 
 fn read_message(stream: &TcpStream) -> Result<String, std::io::Error> {
@@ -175,23 +160,16 @@ fn read_message(stream: &TcpStream) -> Result<String, std::io::Error> {
     }
 }
 
-fn broadcast_message(
-    sender_addr: &SocketAddr,
-    message: &str,
-    system_message: bool,
-) -> Result<(), std::io::Error> {
-    let (sender_username, streams) = {
-        let users = USERS.lock().unwrap();
-        let sender_username = users
-            .get(sender_addr)
-            .map(|u| u.username.clone())
-            .unwrap_or_default();
-        let streams = users
+fn broadcast_message(sender_addr: &SocketAddr, message: &str, system_message: bool) {
+    let sender_username = get_username(sender_addr).unwrap_or_default();
+    let streams = {
+        let users = JOINED_USERS.lock().unwrap();
+        users
             .iter()
             .filter(|(addr, _)| !(*addr).eq(sender_addr))
             .map(|(_, user)| user.stream.try_clone())
-            .collect::<Result<Vec<_>, _>>()?;
-        (sender_username, streams)
+            .collect::<Result<Vec<_>, _>>()
+            .unwrap_or_default()
     };
 
     let full_message = if system_message {
@@ -200,12 +178,13 @@ fn broadcast_message(
         format!("[{}] {}", sender_username, message)
     };
     for stream in streams {
-        send_message(&stream, &full_message)?;
+        // We don't care if sending the message fails
+        // If a user is disconnected, we'll discover it in the handle_connection function and clean up there
+        let _ = send_message(&stream, &full_message);
     }
-    Ok(())
 }
 
 fn get_username(peer_addr: &SocketAddr) -> Option<String> {
-    let users = USERS.lock().unwrap();
+    let users = JOINED_USERS.lock().unwrap();
     users.get(peer_addr).map(|u| u.username.clone())
 }
